3129b851193ab7bcf0ccd9df13ddb7b640c62de8
Tim Roberts <timr@probo.com>
2018-12-21 15:37:51 -0800

    Add some comments.

--- 2018/day20.py
+++ 2018/day20.py
@@ -13,10 +13,32 @@ def printgraph(maze):
     wid = e-w+1
     hgt = s-n+1
 
-    for y in range(n,s+1):
-        for x in range(w,e+1):
-            print( maze[x+y*1j], end=' | ')
-        print()
+    basewall = ['#','#'] * wid + ['#']
+    baseroom = ['#','.'] * wid + ['#']
+    for y in range(hgt):
+        wall = basewall[:]
+        room = baseroom[:]
+        for x in range(wid):
+            me = (x+w)+(y+n)*1j
+            nn = me - 1j
+            ww = me - 1
+            if nn in maze[me]:
+                wall[x+x+1] = "-"
+            if ww in maze[me]:
+                room[x+x] = "|"
+        print( ''.join(wall) ) 
+        print( ''.join(room) )
+            
+    print(''.join(basewall))
+
+# So, here's the magic.  Rather than build all of the possible strings
+# up to this point, pos simply holds the list of coordinates we might be
+# at, based on all the alternatives to this point.  When we get an 
+# alternative, we start again with the list of starting positions for 
+# this set of alternatives.
+#
+# That's really clever.
+
 
 def createMaze( paths ):
     maze = defaultdict(set)
@@ -24,57 +46,57 @@ def createMaze( paths ):
     stack = []  # a stack keeping track of (starts, ends) for groups
     starts, ends = {0}, {0}   # current possible starting and ending positions
 
-    points = []
     for c in paths:
         if c == '|':
-            # an alternate: update possible ending points, and restart the group
+            # An alternate: update possible ending points, and restart the group
             ends.update(pos)
             pos = starts
         elif c in 'NESW':
-            # move in a given direction: add all edges and update our current positions
-            direction = {'N': 1, 'E': 1j, 'S': -1, 'W': -1j}[c]
-#            print( tuple((p,p+direction) for p in pos) )
+            # Move in a given direction, by updating all of possible positions
+            # we might have started at.  Add these edges to the graph.
+            direction = {'N': -1j, 'E': 1, 'S': 1j, 'W': -1}[c]
             for p in pos:
                 maze[p].add( p+direction )
                 maze[p+direction].add( p )
-            pos = {p + direction for p in pos}
+            pos = {p+direction for p in pos}
         elif c == '(':
-            # start of group: add current positions as start of a new group
+            # Start of group.  Push the current starting set so we can use
+            # it later, and start a new group.
             stack.append((starts, ends))
             starts, ends = pos, set()
         elif c == ')':
-            # end of group: finish current group, add current positions as possible ends
+            # End of group.  Add the current positions as possible endpoints,
+            # and pop the last positions off the stack.
             starts, ends = stack.pop()
             ends.update(pos)
     return maze
 
+# Determine the path lengths.
+
+def shortestPaths(graph):
+    lengths = {0:0}
+    visits = [0]
+    steps = 0
+    while visits:
+        steps += 1
+        nextstep = []
+        for v in visits:
+            for p in graph[v]:
+                if p not in lengths:
+                    lengths[p] = steps
+                    nextstep.append( p )
+        visits = nextstep
+    return lengths
+
 # Build the graph of all possible door movements.
 
 graph = createMaze( sys.stdin.read() )
-
-print( len(graph) )
-
-# Determine the path lengths.
-
-lengths = defaultdict(int)
-
-visits = [0]
-lengths[0] = 0
-steps = 0
-while visits:
-    steps += 1
-    nextstep = []
-    for v in visits:
-        print(v, graph[v])
-        for p in graph[v]:
-            if p not in lengths:
-                lengths[p] = steps
-                nextstep.append( p )
-    visits = nextstep
-
-print( lengths )
+#printgraph(graph)
 
 # find the shortest path lengths from the starting room to all other rooms
+
+lengths = shortestPaths( graph )
+#print( lengths )
 
 print('part1:', max(lengths.values()))
 print('part2:', sum(1 for length in lengths.values() if length >= 1000))
