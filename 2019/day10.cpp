3798c83cf2d528c44f069c4e0083ff9e012c1310
Tim Roberts <timr@probo.com>
2019-12-10 22:22:18 -0800

    Day10 now works.

--- 2019/day10.cpp
+++ 2019/day10.cpp
@@ -4,88 +4,92 @@
 #include <sstream>
 #include <cmath>
 #include <vector>
+#include <tuple>
 #include <set>
 #include <algorithm>
+
+#ifndef M_PI
 #define M_PI 3.14159265358979
+#endif
 
 // 3,4 with 8
 
 const char * t1 = "\
-.#..#\
-.....\
-#####\
-....#\
-...##\
+.#..#\n\
+.....\n\
+#####\n\
+....#\n\
+...##\n\
 ";
 
 // 5,6 with 33
 
 const char * t2 = "\
-#..#.#....\
-..#######.\
-.#.#.###..\
-.#..#.....\
-..#....#.#\
-#..#....#.\
-.##.#..###\
-##...#..#.\
-.#....####\
+#..#.#....\n\
+..#######.\n\
+.#.#.###..\n\
+.#..#.....\n\
+..#....#.#\n\
+#..#....#.\n\
+.##.#..###\n\
+##...#..#.\n\
+.#....####\n\
 ";
 
 
 // 1,2 with 35
 
 const char * t3 = "\
-#.#...#.#.\
-.###....#.\
-.#....#...\
-##.#.#.#.#\
-....#.#.#.\
-.##..###.#\
-..#...##..\
-..##....##\
-......#...\
-.####.###.\
+#.#...#.#.\n\
+.###....#.\n\
+.#....#...\n\
+##.#.#.#.#\n\
+....#.#.#.\n\
+.##..###.#\n\
+..#...##..\n\
+..##....##\n\
+......#...\n\
+.####.###.\n\
 ";
 
 // 6,3 with 41
 
 const char * t4 = "\
-.#..#..###\
-####.###.#\
-....###.#.\
-..###.##.#\
-##.##.#.#.\
-....###..#\
-..#.#..#.#\
-#..#.#.###\
-.##...##.#\
-.....#.#..\
+.#..#..###\n\
+####.###.#\n\
+....###.#.\n\
+..###.##.#\n\
+##.##.#.#.\n\
+....###..#\n\
+..#.#..#.#\n\
+#..#.#.###\n\
+.##...##.#\n\
+.....#.#..\n\
 ";
 
 // 11,13 with 210
 
 const char * t5 = "\
-.#..##.###...#######\
-##.############..##.\
-.#.######.########.#\
-.###.#######.####.#.\
-#####.##.#.##.###.##\
-..#####..#.#########\
-####################\
-#.####....###.#.#.##\
-##.#################\
-#####.##.###..####..\
-..######..##.#######\
-####.##.####...##..#\
-.#####..#.######.###\
-##...#.##########...\
-#.##########.#######\
-.####.#.###.###.#.##\
-....##.##.###..#####\
-.#.#.###########.###\
-#.#.#.#####.####.###\
-###.##.####.##.#..##\
+.#..##.###...#######\n\
+##.############..##.\n\
+.#.######.########.#\n\
+.###.#######.####.#.\n\
+#####.##.#.##.###.##\n\
+..#####..#.#########\n\
+####################\n\
+#.####....###.#.#.##\n\
+##.#################\n\
+#####.##.###..####..\n\
+..######..##.#######\n\
+####.##.####...##..#\n\
+.#####..#.######.###\n\
+##...#.##########...\n\
+#.##########.#######\n\
+.####.#.###.###.#.##\n\
+....##.##.###..#####\n\
+.#.#.###########.###\n\
+#.#.#.#####.####.###\n\
+###.##.####.##.#..##\n\
 ";
 
 typedef std::vector<std::vector<char>> spacemap_t;
@@ -106,7 +110,7 @@ spacemap_t read( const char * t )
     return map;
 }
 
-spacemap_t read( std::istream & t )
+spacemap_t read( std::istream  & t )
 {
     spacemap_t map;
     for( std::string ln; getline( t, ln); )
@@ -124,14 +128,14 @@ spacemap_t read( std::istream & t )
 struct Point {
     int x;
     int y;
-    Point( int _x, int _y )
+    Point( int _x=0, int _y=0 )
         : x(_x)
         , y(_y)
         {}
 
     bool operator< (const Point & other) const
     {
-        return (abs(x)+abs(y)) < (abs(other.x)+abs(other.y));
+        return x < other.x || y < other.y;
     }
 
     bool operator== (const Point & other) const
@@ -199,10 +203,11 @@ bool visible( spacemap_t & graph, Point p0, Point p1 )
 
 std::set<Point> check( spacemap_t & graph, Point(target) )
 {
-//    std::cout << "Checking" << target.x << " " <<  target.y << "\n";
+//    std::cout << "Checking " << target.x << " " <<  target.y << "\n";
     int dim = graph.size();
     std::set<Point> found;
     for( int y1=0; y1 < dim; y1++ )
+    {
         for( int x1=0; x1 < dim; x1++ )
         {
             Point pt(x1,y1);
@@ -211,22 +216,25 @@ std::set<Point> check( spacemap_t & graph, Point(target) )
             if( graph[y1][x1] != '#' )
                 continue;
             if( visible( graph, target, pt ) )
+            {
+//                std::cout << "   " << pt.x << "," << pt.y << " visible\n";
                 found.insert( pt );
+            }
         }
-//  std::cout << found.size() << "\n";
+    }
     return found;
 }
 
 
 
-int scan(spacemap_t graph) 
+std::pair<int,Point> scan(spacemap_t graph) 
 {
     int maxcount = 0;
     Point maxelem(0,0);
     for( int y = 0; y < graph.size(); y++ )
     {
         auto ln = graph[y];
-        for( int x = 0; y < ln.size(); x++ )
+        for( int x = 0; x < ln.size(); x++ )
         {
             char cell = ln[x];
 //            print( x, y, ln, cell )
@@ -241,7 +249,7 @@ int scan(spacemap_t graph)
         }
     }
     std::cout << maxelem.x << "," << maxelem.y << " sees " << maxcount << "\n";
-    return maxcount; //, maxelem;
+    return std::make_pair( maxcount, maxelem );
 }
 
 
@@ -249,47 +257,42 @@ int scan(spacemap_t graph)
 int laser(spacemap_t graph, Point target)
 {
     int remains = 200;
-    while( remains )
-    {
-        std::set<Point> found = check(graph,target);
+    std::set<Point> found;
+
 // If we don't catch them all in one round, eliminate these.
-        if( found.size() < remains )
-        {
-            for( auto pt : found )
-                graph[pt.y][pt.x] = '.';
-            remains -= found.size();
-            continue;
-        }
-        break;
 
-        auto angle = [&target](Point& pt) {
-            return M_PI/4 - atan2((pt.x-target.x),(pt.y-target.y));
-        };
-
-        // Convert to a vector.
-
-        std::vector<Point> data;
-        data.resize( found.size() );
-        std::copy( found.begin(), found.end(), data.begin() );
-
-        std::sort(
-            data.begin(),
-            data.end(),
-            [&target, angle](Point & a, Point & b) {
-                return angle(a) < angle(b);
-            } 
-        );
-
-        std::cout << "Number 200 is " 
-            << data[remains-1].x << ","
-            << data[remains-1].y << "\n";
-
-        return data[remains-1].y * 100 + data[remains-1].x;
+    while( (found = check(graph,target)).size() < remains )
+    {
+        for( auto pt : found )
+            graph[pt.y][pt.x] = '.';
+        remains -= found.size();
     }
-    return 0;
+
+    auto angle = [&target](Point& pt) {
+        return M_PI/4 - atan2((pt.x-target.x),(pt.y-target.y));
+    };
+
+    // Convert to a vector.
+
+    std::vector<Point> data;
+    data.resize( found.size() );
+    std::copy( found.begin(), found.end(), data.begin() );
+
+    std::sort(
+        data.begin(),
+        data.end(),
+        [&target, angle](Point & a, Point & b) {
+            return angle(a) < angle(b);
+        } 
+    );
+
+    std::cout << "Number 200 is " 
+        << data[remains-1].x << ","
+        << data[remains-1].y << "\n";
+
+    return data[remains-1].x * 100 + data[remains-1].y;
 }
 
-    
 
 void part1()
 {
@@ -298,17 +301,22 @@ void part1()
     scan(read(t3));
     scan(read(t4));
     scan(read(t5));
-    std::cout << "Part 1: " << scan(read(std::ifstream("day10.txt")))[0] << "\n";
+    std::ifstream real("day10.txt");
+    std::cout << "Part 1: " << scan(read(real)).first << "\n";
 }
 
 // Answer was 26, 29.
         
 void part2()
 {
-    Point best = scan(read(t5))[1];
-    laser( read(t5), best );
-    best = scan(read(std::ifstream("day10.txt")))[1];
-    std::cout << "\nPart 2: " << laser(read(std::ifstream("day10.txt")), best)  << "\n";
+    Point best = scan(read(t5)).second;
+    std::cout << laser( read(t5), best ) << "\n";
+
+    std::ifstream real("day10.txt");
+    best = scan(read(real)).second;
+    real.close();
+    real.open("day10.txt");
+    std::cout << "\nPart 2: " << laser(read(real), best)  << "\n";
 }
 
 int main()
