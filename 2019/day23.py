461cf3641e4fe90148572209c56532d5c0305ddf
Tim Roberts <timr@probo.com>
2019-12-22 22:29:34 -0800

    Add day 23.

---
+++ 2019/day23.py
@@ -0,0 +1,77 @@
+import sys
+import time
+import queue
+from intcode import Program
+from tools import Point
+
+TRACE = 'trace' in sys.argv
+
+progs = []
+nat = None
+
+class Prog23(Program):
+    def read_input(self):
+        if self.nextin is not None:
+            self.idle = False
+            i, self.nextin = self.nextin, None
+            return i
+        if not self.input.empty():
+            self.idle = False
+            ip = self.input.get()
+            if isinstance( ip, int ):
+                return ip
+            a,self.nextin = ip
+            if TRACE:
+                print( f"{self.id}: read {a} {self.nextin}" )
+            return a
+        time.sleep( 0.1 )
+        self.idle = True
+        return -1
+
+    def send_output(self,p):
+        global nat
+        if self.output.qsize() > 1:
+            ad = self.output.get()
+            x = self.output.get()
+            if TRACE:
+                print( f"{self.id}: send {x} {p} to {ad}" )
+            pkt = (x,p)
+            if ad == 255:
+                if TRACE:
+                    print( "Sending", pkt )
+                nat = pkt
+                return
+            progs[ad].push( pkt )
+        else:
+            self.output.put( p )
+
+real = list(eval(open('day23.txt').read()))
+
+for i in range(50):
+    progs.append( Prog23(real) )
+    progs[-1].nextin = i
+
+for i in range(50):
+    progs[i].start()
+
+
+lastnat = None
+while 1:
+    # Wait for idle.
+    time.sleep( 0.1 )
+    if nat and all(p.idle for p in progs):
+        if not lastnat:
+            print( "Part 1:", nat )
+        if nat == lastnat:
+            print( "Part 2:", nat )
+            break
+        if TRACE:
+            print( "Delivering", nat )
+        progs[0].push( nat[0] )
+        progs[0].push( nat[1] )
+        nat,lastnat = None,nat
+
+for p in progs:
+    p.die = True
+
+time.sleep( 0.5 )
