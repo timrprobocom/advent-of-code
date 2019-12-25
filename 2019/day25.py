e3d164888c12e2921c789ec8e55476733fd8acbf
Tim Roberts <timr@probo.com>
2019-12-24 21:49:58 -0800

    Check in day 25.

---
+++ 2019/day25.py
@@ -0,0 +1,28 @@
+import sys
+from intcode import Program
+from tools import Point
+
+
+
+TRACE = 'trace' in sys.argv
+
+# Run the program once to get the path.
+
+class Prog25(Program):
+    def __init__(self,pgm):
+        Program.__init__(self,pgm)
+        self.string = []
+
+    def read_input(self):
+        if not self.string:
+            answer = input("Input: ")
+            self.string = list(ord(s) for s in answer+'\n' )
+        return int(self.string.pop(0))
+         
+    def send_output(self,x):
+        sys.stdout.write(chr(x))
+
+real = list(eval(open('day25.txt').read()))
+pgm = Prog25(real)
+pgm.run()
+print( ''.join(chr(c) for c in pgm.dump()) )
