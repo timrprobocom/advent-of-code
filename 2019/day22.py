1f161212ff97d7ccd3e0ccfd8dd699a7469dd9fa
Tim Roberts <timr@probo.com>
2019-12-27 17:01:35 -0800

    Add a better modulo invert operator.

--- 2019/day22.py
+++ 2019/day22.py
@@ -4,14 +4,39 @@ import functools
 
 TRACE = 'trace' in sys.argv
 
-# Modular inverse
+# Find the multiplicative inverse in a modulus field.
 
-def inv(mod, n):
-    modp = mod - 2
-    if modp == 8:
-        modp -= 1
-    return pow(n, modp, mod)
+# This is Euler's theorem.  This is slick, but it really only works if
+# mod is prime.  The first line computes the "totient" of the modulus,
+# which is mod-1 if mod is prime, but requires prime factoring otherwise.
+# The test case uses mod 10 which, of course, is not prime.  We hardcode 
+# the totient for 10 to allow the test to work.
 
+def invert_euler(n, mod):
+    tot = 4 if mod == 10 else mod - 1
+    return pow(n, tot-1, mod)
+
+# This is the Extended Euclidean Algorithm.  It does not care about the
+# totient.  It does, however, fail if a is negative, so we compensate.
+# It is basically doing a GCD.
+
+def invert_euclid(a, n):
+    t = 0; newt = 1
+    r = n; newr = a if a > 0 else a+n
+
+    while newr:
+        q = r // newr
+        (t,newt) = (newt, t - q * newt)
+        (r,newr) = (newr, r - q * newr)
+    if r > 1:
+        print( a, "is not invertable mod", b )
+        sys.exit(0)
+    if t < 0:
+        t += n
+    return t
+
+invert = invert_euclid
+ 
 
 test1 = """\
 deal into new stack
@@ -26,8 +51,8 @@ deal with increment 3
 cut -1"""
 # Result: 9 2 5 8 1 4 7 0 3 6
 
-# A deck can be encoded as ax+b mod n.
 
+# A deck can be encoded as ax+b mod n.
 
 class Deck(object):
     def __init__(self,k):
@@ -36,16 +61,14 @@ class Deck(object):
 
     def cut(self,n):
         off = (self.state[0] + self.state[1] * n) % self.count
-        self.state = (off, self.state[1] )
+        self.state = (off, self.state[1])
 
     def reverse(self):
         inc = -self.state[1] 
         self.state = ((self.state[0]+inc+self.count) % self.count, inc)
 
     def increment(self,n):
-    # We need count-3 for 10, but count-2 for 10007.  Why?
-
-        inc = (self.state[1] * inv(self.count, n)) % self.count
+        inc = (self.state[1] * invert(n, self.count)) % self.count
         self.state = (self.state[0], inc)
 
     def run( self, src ):
@@ -62,7 +85,7 @@ class Deck(object):
             if TRACE:
                 print( self.state )
 
-# Do a part 1 test.
+# Do a part 1 test.  Should be 9 2 5 8 1 4 7 0 3 6, or 9+3x.
 
 COUNT = 10
 deck = Deck(COUNT)
@@ -73,7 +96,7 @@ for x in range(COUNT):
     print( (m*x+b) % COUNT, end='' )
 print( '' )
 
-# Do part 1.
+# Do part 1.  Should be 4775.
 
 COUNT = 10007
 real = open("day22.txt").readlines()
@@ -88,7 +111,7 @@ for x in range(COUNT):
         print( "Part 1:", x );
         break
 
-# Do part 2.
+# Do part 2.  Shouild be 37889219674304.
 
 COUNT = 119315717514047
 
@@ -106,7 +129,8 @@ b,m = deck.state
 
 cycles = 101741582076661
 finalm = pow( m, cycles, COUNT )
-finalb = (b * (1 - finalm) * inv(COUNT, 1 - m)  ) % COUNT
+finalb = (b * (1 - finalm) * invert(1 - m, COUNT)  ) % COUNT
+print(finalm,finalb)
 
 tgt = 2020
 print( "Part 2:", (finalm * tgt + finalb) % COUNT )
