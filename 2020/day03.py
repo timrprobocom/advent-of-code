c8d499b5c40764b0cf3925ee6c7d326362d71443
Tim Roberts <timr@probo.com>
2020-12-02 21:26:13 -0800

    Add a second attempt.

--- 2020/day03.py
+++ 2020/day03.py
@@ -22,19 +22,38 @@ else:
 
 # This could have been done smarter.
 
-counts = [0,0,0,0,0]
-x = [0,0,0,0,0]
-dx = [1,3,5,7,1]
-dy = [1,1,1,1,2]
+def try1():
+    counts = [0,0,0,0,0]
+    x = [0,0,0,0,0]
+    dx = [1,3,5,7,1]
+    dy = [1,1,1,1,2]
 
-for y,ln in enumerate(data):
-    for i in range(len(x)):
-        if y % dy[i]:
-            continue
-        if ln[x[i]] == '#':
-            counts[i] += 1
-        x[i] = (x[i] + dx[i]) % len(ln)
+    for y,ln in enumerate(data):
+        for i in range(len(x)):
+            if y % dy[i]:
+                continue
+            if ln[x[i]] == '#':
+                counts[i] += 1
+            x[i] = (x[i] + dx[i]) % len(ln)
 
-print( counts )
+    return counts
+    print( "Part 1:", counts[1] )
+    print( "Part 2:", counts[0] * counts[1] * counts[2] * counts[3] * counts[4] )
+
+def counttrees( dx, dy ):
+    x,y = 0,0
+    count = 0
+    while y < len(data):
+        if data[y][x] == '#':
+            count += 1
+        x = (x + dx) % len(data[0])
+        y += dy
+    return count
+
+def try2():
+    return [counttrees(*s) for s in ((1,1),(3,1),(5,1),(7,1),(1,2))]
+
+counts = try2()
 print( "Part 1:", counts[1] )
 print( "Part 2:", counts[0] * counts[1] * counts[2] * counts[3] * counts[4] )
+
