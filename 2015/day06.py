b9f6e977d408fc26de8c15f6b6052c870dbd27bf
Tim Roberts <timr@probo.com>
2021-01-08 15:54:11 -0800

    Use numpy.  Better, faster.

--- 2015/day06.py
+++ 2015/day06.py
@@ -1,50 +1,44 @@
+import numpy as np
 
 
+def xy0(s):
+    x = s.split(',')
+    return int(x[0]),int(x[1])
 
-def xy(s):
-    return tuple( int(i) for i in s.split(',') )
+def xy1(s):
+    x = s.split(',')
+    return int(x[0])+1,int(x[1])+1
 
-def set(grid,tl,br,val):
-    for y in range(tl[1],br[1]+1):
-        for x in range(tl[0],br[0]+1):
-            grid[y][x] = val
+def setx(grid,tl,br,val):
+    grid[tl[0]:br[0],tl[1]:br[1]] = val
 
 def tog(grid,tl,br):
-    for y in range(tl[1],br[1]+1):
-        for x in range(tl[0],br[0]+1):
-            grid[y][x] = 1 - grid[y][x]
+    grid[tl[0]:br[0],tl[1]:br[1]] = 1 - grid[tl[0]:br[0],tl[1]:br[1]]
 
 def bump(grid,tl,br,val):
-    delta = 2 * val - 1
-    for y in range(tl[1],br[1]+1):
-        for x in range(tl[0],br[0]+1):
-            grid[y][x] = max( 0, grid[y][x]+delta )
+    grid[tl[0]:br[0],tl[1]:br[1]] = np.clip( grid[tl[0]:br[0],tl[1]:br[1]] + 2 * val - 1, 0, 999 )
 
 def bump2(grid,tl,br):
-    for y in range(tl[1],br[1]+1):
-        for x in range(tl[0],br[0]+1):
-            grid[y][x] += 2
+    grid[tl[0]:br[0],tl[1]:br[1]] += 2
 
-def process( set, tog ):
-    grid = []
-    for i in range(1000):
-        grid.append( [0]*1000 )
+def process( setx, tog ):
+    grid = np.zeros((1000,1000),dtype=np.int32)
     for ln in open('day06.txt'):
         words = ln.strip().split()
         if words[0] == 'toggle':
-            tl = xy(words[1])
-            br = xy(words[3])
+            tl = xy0(words[1])
+            br = xy1(words[3])
             tog( grid, tl, br )
         elif words[1] == 'on':
-            tl = xy(words[2])
-            br = xy(words[4])
-            set( grid, tl, br, 1 )
+            tl = xy0(words[2])
+            br = xy1(words[4])
+            setx( grid, tl, br, 1 )
         elif words[1] == 'off':
-            tl = xy(words[2])
-            br = xy(words[4])
-            set( grid, tl, br, 0 )
+            tl = xy0(words[2])
+            br = xy1(words[4])
+            setx( grid, tl, br, 0 )
 
-    return sum( sum(row) for row in grid )
+    return grid.sum()
 
-print( "Part 1:", process( set, tog ) )
-print( "Part 1:", process( bump, bump2 ) )
+print( "Part 1:", process( setx, tog ) )
+print( "Part 2:", process( bump, bump2 ) )
