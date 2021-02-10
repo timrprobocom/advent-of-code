85406b932085d50fa31251768cb857d4a725a0da
Tim Roberts <timr@probo.com>
2021-02-09 16:44:27 -0800

    Better file I/O in Rust.

--- 2020/day01.rs
+++ 2020/day01.rs
@@ -1,6 +1,3 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 
 fn pass1 ( values: &Vec<i32> ) -> i32 {
     for t1 in values {
@@ -10,7 +7,7 @@ fn pass1 ( values: &Vec<i32> ) -> i32 {
             }
         }
     }
-    return 0;
+    0
 }
 
 fn pass2 ( values: &Vec<i32> ) -> i32 {
@@ -23,34 +20,21 @@ fn pass2 ( values: &Vec<i32> ) -> i32 {
             }
         }
     }
-    return 0;
+    0
 }
 
-
-fn main() {
+pub fn main() {
 
     let test = vec![ 1721, 979, 366, 299, 675, 1456 ];
     println!( "{}", pass1( &test ));
     println!( "{}", pass2( &test ));
 
-    let mut live: Vec<i32> = Vec::new();
-    if let Ok(lines) = read_lines("day01.txt") {
-        for line in lines {
-            if let Ok(ln) = line {
-                if let Ok(amt) = ln.parse::<i32>() {
-                    live.push( amt );
-                }
-            }
-        }
-    }
+    let live = include_str!("day01.txt")
+        .lines()
+        .map( |i| i.parse::<i32>().unwrap() )
+        .collect();
+
     println!( "Pass 1: {}", pass1( &live ));
     println!( "Pass 2: {}", pass2( &live ));
 }
 
-// What the hell is this doing?
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
