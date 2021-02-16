c8addc653ee2e45b2684f4a7ebab3d7c25712855
Tim Roberts <timr@probo.com>
2021-02-16 15:31:06 -0800

    Begins to make a little more sense.

--- 2020/day02rs/src/main.rs
+++ 2020/day02rs/src/main.rs
@@ -1,23 +1,30 @@
 use regex::{Regex,Captures};
 use lazy_static::lazy_static;
 
-fn parse<'a>( caps : &'a Captures ) -> (usize, usize, char, &'a str) {
-    (
-        caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-        caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
-        caps.get(3).unwrap().as_str().chars().next().unwrap(),
-        caps.get(4).unwrap().as_str()
-    )
+struct Problem {
+    a: usize,
+    b: usize,
+    tgt: char,
+    hay: String
 }
 
-fn pass1( a : usize, b : usize, tgt : char, hay : &str) -> bool {
-    let cnt = hay.matches(tgt).count();
-    a <= cnt && cnt <= b
+fn parse( caps : &Captures ) -> Problem {
+    Problem {
+        a:   caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+        b:   caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+        tgt: caps.get(3).unwrap().as_str().chars().next().unwrap(),
+        hay: caps.get(4).unwrap().as_str().to_string()
+    }
 }
 
-fn pass2( a : usize, b : usize, tgt : char, hay0 : &str) -> bool {
-    let hay : Vec<char> = hay0.chars().collect();
-    (hay[a-1] == tgt) != (hay[b-1] == tgt)
+fn pass1( p : &Problem ) -> bool {
+    let cnt = p.hay.matches(p.tgt).count();
+    p.a <= cnt && cnt <= p.b
+}
+
+fn pass2( p : &Problem ) -> bool {
+    let hay : Vec<char> = p.hay.chars().collect();
+    (hay[p.a-1] == p.tgt) != (hay[p.b-1] == p.tgt)
 }
 
 fn main() -> std::io::Result<()> {
@@ -36,23 +43,22 @@ fn main() -> std::io::Result<()> {
         ).unwrap();
     }
 
-    let live : Vec<Captures> = include_str!("../../day02.txt")
+    let live : Vec<Problem> = include_str!("../../day02.txt")
         .lines()
         .map(|line| MYRE.captures(line).unwrap())
+        .map(|x| parse(&x) )
         .collect();
 
     let part1 = live
         .iter()
-        .map(|x| parse(x) )
-        .filter(|(a,b,c,d)| pass1(*a,*b,*c,d) )
+        .filter(|x| pass1(x) )
         .count();
 
     println!("Part 1: {}", part1 );
 
     let part2 = live
         .iter()
-        .map(|x| parse(x) )
-        .filter(|(a,b,c,d)| pass2(*a,*b,*c,d) )
+        .filter(|x| pass2(x) )
         .count();
 
     println!("Part 2: {}", part2 );
