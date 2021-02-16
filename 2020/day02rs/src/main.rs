ec2c8e8a4f0de69bfc5bb833e7e2b092d4301d90
Tim Roberts <timr@probo.com>
2021-02-16 15:19:20 -0800

    Separate the parsing.

--- 2020/day02rs/src/main.rs
+++ 2020/day02rs/src/main.rs
@@ -1,39 +1,34 @@
 use regex::{Regex,Captures};
 use lazy_static::lazy_static;
 
-fn parse<'a>( caps : &'a Captures ) -> (usize, usize, &'a str, &'a str) {
-    let a = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-    let b = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-    let tgt = caps.get(3).unwrap().as_str();
-    let hay = caps.get(4).unwrap().as_str();
-    (a, b, tgt, hay)
+fn parse<'a>( caps : &'a Captures ) -> (usize, usize, char, &'a str) {
+    (
+        caps.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+        caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+        caps.get(3).unwrap().as_str().chars().next().unwrap(),
+        caps.get(4).unwrap().as_str()
+    )
 }
 
-fn pass1( caps : &Captures ) -> bool {
-    let a = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-    let b = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-    let tgt = caps.get(3).unwrap().as_str();
-    let hay = caps.get(4).unwrap().as_str();
-//fn pass1( a : usize, b : usize, tgt : &str, hay : &str) -> bool {
+fn pass1( a : usize, b : usize, tgt : char, hay : &str) -> bool {
     let cnt = hay.matches(tgt).count();
     a <= cnt && cnt <= b
 }
 
-fn pass2( caps : &Captures ) -> bool {
-    let a = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-    let b = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-    let tgt = caps.get(3).unwrap().as_str().chars().next().unwrap();
-    let hay : Vec<char> = caps.get(4).unwrap().as_str().chars().collect();
+fn pass2( a : usize, b : usize, tgt : char, hay0 : &str) -> bool {
+    let hay : Vec<char> = hay0.chars().collect();
     (hay[a-1] == tgt) != (hay[b-1] == tgt)
 }
 
 fn main() -> std::io::Result<()> {
 
+/*
     let _test = vec![
         "1-3 a: abcde",
         "1-3 b: cdefg",
         "2-9 c: ccccccccc"
     ];
+*/
 
     lazy_static! {
         static ref MYRE : Regex = Regex::new( 
@@ -41,18 +36,23 @@ fn main() -> std::io::Result<()> {
         ).unwrap();
     }
 
-    let part1 = include_str!("../../day02.txt")
+    let live : Vec<Captures> = include_str!("../../day02.txt")
         .lines()
         .map(|line| MYRE.captures(line).unwrap())
-        .filter(|x| pass1(x) )
+        .collect();
+
+    let part1 = live
+        .iter()
+        .map(|x| parse(x) )
+        .filter(|(a,b,c,d)| pass1(*a,*b,*c,d) )
         .count();
 
     println!("Part 1: {}", part1 );
 
-    let part2 = include_str!("../../day02.txt")
-        .lines()
-        .map(|line| MYRE.captures(line).unwrap())
-        .filter(|x| pass2(x) )
+    let part2 = live
+        .iter()
+        .map(|x| parse(x) )
+        .filter(|(a,b,c,d)| pass2(*a,*b,*c,d) )
         .count();
 
     println!("Part 2: {}", part2 );
