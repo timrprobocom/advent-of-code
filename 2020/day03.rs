36f296df3102f3e6e47bde3f9e94541cc8f76c98
Tim Roberts <timr@probo.com>
2021-02-17 15:50:31 -0800

    Make day 3 rust more idiomatic.

--- 2020/day03.rs
+++ 2020/day03.rs
@@ -1,14 +1,9 @@
+use std::env;
 
 fn generate ( dx: usize, dy: usize, limx: usize, limy: usize ) -> Vec<(usize,usize)> {
-    let mut x : usize = 0;
-    let mut y : usize = 0;
-    let mut coords = Vec::<(usize,usize)>::new();
-    while y < limy {
-        coords.push( (x,y) );
-        x = (x + dx) % limx;
-        y += dy
-    }
-    coords
+    (0..).take_while(|i| *i*dy < limy)
+        .map(|i| ((i*dx)%limx, i*dy))
+        .collect()
 }
 
 fn import ( incoming : Vec::<&str> ) -> Vec<Vec<char>> {
@@ -26,29 +21,34 @@ fn counttrees( grid : &Vec<Vec<char>>, dx: usize, dy: usize ) -> usize {
 }
 
 pub fn main() {
-    let _test = import( vec![
-        "..##.......",
-        "#...#...#..",
-        ".#....#..#.",
-        "..#.#...#.#",
-        ".#...##..#.",
-        "..#.##.....",
-        ".#.#.#....#",
-        ".#........#",
-        "#.##...#...",
-        "#...##....#",
-        ".#..#...#.#"
-    ]);
+    let live = if env::args().any(|x| x == "test") 
+    {
+        import( vec![
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#"
+        ])
+    } else {
+        include_str!("day03.txt")
+            .lines()
+            .map(|x| x.chars().collect())
+            .collect()
+    };
 
-    let live : Vec<Vec<char>> = include_str!("day03.txt")
-        .lines()
-        .map(|x| x.chars().collect())
+    let counts : Vec<usize> = 
+        [(1,1),(3,1),(5,1),(7,1),(1,2)]
+        .iter()
+        .map(|(x,y)| counttrees( &live, *x, *y ) )
         .collect();
 
-    let mut counts = Vec::<usize>::new();
-    for (x,y) in vec![(1,1),(3,1),(5,1),(7,1),(1,2)] {
-        counts.push( counttrees( &live, x, y ) );
-    }
 
     println!("Part 1: {}", counts[1] );
     println!("Part 2: {}", counts.iter().fold(1, |tot,i| tot * i ) );
