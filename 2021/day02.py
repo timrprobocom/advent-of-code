445b034b9ea007e7c86928150b6c6e44739f7492
Tim Roberts <timr@probo.com>
2021-12-03 10:48:54 -0800

    Pointless improvements.

--- 2021/day02.py
+++ 2021/day02.py
@@ -1,4 +1,5 @@
 import sys
+from dataclasses import dataclass
 
 test = """\
 forward 5
@@ -8,11 +9,21 @@ up 3
 down 8
 forward 2"""
 
-
 if 'test' in sys.argv:
     idata = test.splitlines()
 else:
     idata = open('day02.txt').readlines()
+
+@dataclass
+class Position:
+    pos : int = 0
+    depth : int = 0
+    aim : int = 0
+
+    def __repr__(self):
+        return f"<Position {self.pos} {self.depth} {self.aim}>"
+
+# Parse the directions.
 
 data = []
 for row in idata:
@@ -20,34 +31,32 @@ for row in idata:
     data.append( (parts[0], int(parts[1])) )
 
 
-def pass1(data):
-    pos = [0,0]
+def part1(data):
+    pos = Position()
     for verb,cnt in data:
         if verb == 'forward':
-            pos = [pos[0]+cnt, pos[1]]
+            pos.pos += cnt
         elif verb == 'up':
-            pos = [pos[0], pos[1]-cnt]
+            pos.depth -= cnt
         elif verb == 'down':
-            pos = [pos[0], pos[1]+cnt]
+            pos.depth += cnt
 
-    return pos, pos[0]*pos[1]
+    return pos.pos * pos.depth
 
 
-def pass2(data):
-    # surface, depth, aim
-    pos = [0,0,0]
+def part2(data):
+    pos = Position()
     for verb,cnt in data:
-        print(pos)
         if verb == 'forward':
-            pos = [pos[0]+cnt, pos[1]+pos[2]*cnt, pos[2]]
+            pos.pos += cnt
+            pos.depth += pos.aim * cnt
         elif verb == 'up':
-            pos = [pos[0], pos[1], pos[2]-cnt]
+            pos.aim -= cnt
         elif verb == 'down':
-            pos = [pos[0], pos[1], pos[2]+cnt]
+            pos.aim += cnt
 
-    return pos, pos[0]*pos[1]
+    return pos.pos * pos.depth
 
-
-print("Part 1:", pass1(data) )
-print("Part 2:", pass2(data) )
+print("Part 1:", part1(data) )
+print("Part 2:", part2(data) )
 
--- 2021/day03.py
+++ 2021/day03.py
@@ -22,53 +22,46 @@ if 'test' in sys.argv:
 else:
     data = [line.rstrip() for line in open('day03.txt')]
 
+# Is it better to keep the input as a string, or as an integer?
+# It's the difference between row[i] and row & bit.
 
 def common(data):
     num = len(data)
-    maxn = len(data[0])
-    counts = [0]*maxn
+    bits = len(data[0])
+    allone = (1 << bits) - 1
+    counts = [0]*bits
     for row in data:
         for i,c in enumerate(row):
-            if c == '1':
-                counts[i] += 1
+            counts[i] += int(c)
     res = 0
     for i in counts:
-        res = res * 2
-        if i*2 >= num:
-            res += 1
+        res = res * 2 + (1 if i*2 >= num else 0)
     if DEBUG:
-        print(num, maxn, counts, res)
-    return res, ((1<<maxn)-res-1)
+        print(num, bits, counts, res)
+    return res, allone-res
 
-def pass1(data):
+def part1(data):
     most, least = common(data)
     return most * least
 
-def pass2(data):
-    maxn = len(data[0])
+def part2(data):
+    bits = len(data[0])
 
     hdata = data
     ldata = data
 
-    for bitno in range(maxn):
-        bit = 1<<(maxn-bitno-1)
+    for bitno in range(bits):
+        bit = 1 << (bits-bitno-1)
         if len(hdata) > 1:
             most, least = common(hdata)
-            hcrit = '1' if bit & most else '0'
-            ndata = []
-            for row in hdata:
-                if row[bitno] == hcrit:
-                    ndata.append( row )
-            hdata = ndata
+            crit = '1' if bit & most else '0'
+            hdata = [row for row in hdata if row[bitno] == crit]
 
         if len(ldata) > 1:
             most, least = common(ldata)
-            lcrit = '1' if bit & least else '0'
-            ndata = []
-            for row in ldata:
-                if row[bitno] == lcrit:
-                    ndata.append( row )
-            ldata =  ndata
+            crit = '1' if bit & least else '0'
+            ldata = [row for row in ldata if row[bitno] == crit]
+
         if len(hdata) == 1 and len(ldata) == 1:
             break
 
@@ -76,6 +69,6 @@ def pass2(data):
     return int(hdata[0],2) * int(ldata[0],2)
 
 
-print("Part 1:", pass1(data) )
-print("Part 2:", pass2(data) )
+print("Part 1:", part1(data) )
+print("Part 2:", part2(data) )
 
