2a9bf364e3e7e06141bcb3f5f7a6603651b58e5b
Tim Roberts <timr@probo.com>
2022-01-25 16:29:20 -0800

    Use ints instead of strings in rust day 3.

--- 2021/day03.rs
+++ 2021/day03.rs
@@ -1,44 +1,39 @@
 use std::env;
 
 
-// Is it better to keep the input as a string, or as an integer?
-// It's the difference between row[i] and row & bit.
-
-fn common( data : &Vec<String> ) -> (u32, u32)
+fn common( bits : usize, data : &Vec<u32> ) -> (u32, u32)
 {
     // We need to enumerate vertically here.
 
     let num: usize = data.len();
-    let bits: usize = data[0].len();
     let mut counts = vec![0; bits];
 
     for row in data {
-        for (i,c) in row.char_indices() {
-            if c == '1' {
+        for i in 0..32 {
+            if row & (1 << i) > 0 {
                 counts[i] += 1;
             }
         }
     }
 
     let mut res : u32 = 0;
-    for i in counts
+    for i in (0..bits).rev()
     {
-        res = res * 2 + if i * 2 >= num { 1 } else { 0 };
+        res = res * 2 + if counts[i] * 2 >= num { 1 } else { 0 };
     }
 
     let allone: u32 = (1 << bits) - 1;
     (res, allone-res)
 }
 
-fn part1( data: &Vec<String> ) -> u32
+fn part1( bits: usize, data: &Vec<u32> ) -> u32
 {
-    let (most, least) = common(data);
+    let (most, least) = common(bits, data);
     most * least
 }
 
-fn part2( data: &Vec<String> ) -> u32
+fn part2( bits: usize, data: &Vec<u32> ) -> u32
 {
-    let bits: usize = data[0].len();
     let mut hdata = data.to_owned();
     let mut ldata = data.to_owned();
 
@@ -46,20 +41,20 @@ fn part2( data: &Vec<String> ) -> u32
         let bit = 1 << (bits-bitno-1);
 
         if hdata.len() > 1 {
-            let (most,_least) = common(&hdata);
-            let crit = if bit & most != 0 {'1'} else {'0'};
+            let (most,_least) = common(bits, &hdata);
+            let crit = bit & most;
             hdata = hdata
                 .into_iter()
-                .filter(|row| row.chars().nth(bitno).unwrap() == crit)
+                .filter(|row| row & bit == crit)
                 .collect();
         }
 
         if ldata.len() > 1 {
-            let (_most,least) = common(&ldata);
-            let crit = if bit & least != 0 {'1'} else {'0'};
+            let (_most,least) = common(bits, &ldata);
+            let crit = bit & least;
             ldata = ldata
                 .into_iter()
-                .filter(|row| row.chars().nth(bitno).unwrap() == crit)
+                .filter(|row| row & bit == crit)
                 .collect();
         }
 
@@ -70,9 +65,7 @@ fn part2( data: &Vec<String> ) -> u32
 
     println!("{} {}", hdata[0], ldata[0]);
 
-    u32::from_str_radix(&hdata[0], 2).unwrap()
-        *
-    u32::from_str_radix(&ldata[0], 2).unwrap()
+    hdata[0] * ldata[0]
 }
 
 
@@ -94,19 +87,28 @@ static TEST : [&str; 12] =
 ];
 
 pub fn main() {
-    let live : Vec<String> = if env::args().any(|x| x == "test")
+    let live : Vec<u32> = if env::args().any(|x| x == "test")
     {
         TEST
             .iter()
-            .map(|x| x.to_string())
+            .map(|x| u32::from_str_radix(x,2).unwrap() )
             .collect()
     } else {
         include_str!("day03.txt")
             .lines()
-            .map(|x| x.to_string())
+            .map(|x| u32::from_str_radix(x,2).unwrap() )
             .collect()
     };
 
-    println!( "Pass 1: {}", part1( &live ));
-    println!( "Pass 2: {}", part2( &live ));
+    let binsum = live.iter().fold(0, |accum,item| accum | item ) + 1;
+    let mut bits : usize = 0;
+    for i in 0..32 {
+        if binsum == 1<<i {
+            bits = i;
+            break;
+        }
+    }
+
+    println!( "Pass 1: {}", part1( bits, &live ));
+    println!( "Pass 2: {}", part2( bits, &live ))
 }
