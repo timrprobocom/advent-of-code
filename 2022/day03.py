ff23d2d4391c52cb7ce74d13a65e4774d00658b4
Tim Roberts <timr@probo.com>
2022-12-02 21:18:09 -0800

    Better day 3.

--- 2022/day03.py
+++ 2022/day03.py
@@ -14,17 +14,19 @@ if 'test' in sys.argv:
 else:
     vals = [s.rstrip() for s in open('day03.txt').readlines()]
 
+def score(setx):
+    char = setx.pop()
+    if char > 'Z':
+        return ord(char)-ord('a')+1
+    else:
+        return ord(char)-ord('A')+27
+
 def part1(vals):
     prior = 0
     for row in vals:
         h = len(row)//2
-        s1 = set(row[:h])
-        s2 = set(row[h:])
-        sect = list(s1.intersection(s2))[0]
-        if sect > 'Z':
-            prior += ord(sect)-ord('a')+1
-        else:
-            prior += ord(sect)-ord('A')+27
+        s1 = set(row[:h]).intersection( set(row[h:]) )
+        prior += score(s1)
     return prior
 
 def part2(vals):
@@ -33,15 +35,12 @@ def part2(vals):
     sect = None
     for row in vals:
         if sect:
-            sect = sect.intersection(set(row))
+            sect.intersection_update(set(row))
         else:
             sect = set(row)
+
         if not counter:
-            sect = list(sect)[0]
-            if sect > 'Z':
-                prior += ord(sect)-ord('a')+1
-            else:
-                prior += ord(sect)-ord('A')+27
+            prior += score(sect)
             sect = None
             counter = 3
         counter -= 1
