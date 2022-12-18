97cbbb5eb6e76bcd69e1d3727c127d9455a050c7
Tim Roberts <timr@probo.com>
2022-12-17 22:23:39 -0800

    Improve day 18.

--- 2022/day18.py
+++ 2022/day18.py
@@ -30,13 +30,22 @@ for line in data:
     dout.add((int(x),int(y),int(z)))
 data = dout
 
+directions = (-1,0,0),(1,0,0),(0,-1,0),(0,1,0),(0,0,-1),(0,0,1)
+
+def around(cube):
+    for delta in directions:
+        yield tuple(a+b for a,b in zip(cube,delta))
+
 def part1(data):
-    cubes = { k:0 for k in data }
-    for c1 in data:
-        for c2 in data:
-            if sum(abs(cc1-cc2) for cc1,cc2 in zip(c1,c2)) == 1:
-                cubes[c1] += 1
-    return 6 * len(cubes) - sum(cubes.values())
+    result = 0
+    seen = set()
+    for cube in data:
+        result += 6
+        for near in around(cube):
+            if near in seen:
+                result -= 2
+        seen.add(cube)
+    return result
 
 minx = min(x for x,y,z in data)
 maxx = max(x for x,y,z in data)
@@ -48,11 +57,6 @@ xrange = range(minx,maxx+1)
 yrange = range(miny,maxy+1)
 zrange = range(minz,maxz+1)
 
-directions = (-1,0,0),(1,0,0),(0,-1,0),(0,1,0),(0,0,-1),(0,0,1)
-
-def add(cube,delta):
-    return tuple(a+b for a,b in zip(cube,delta))
-
 # This eventually builds up a list of all cubes within the x,y,z range
 # that are not surrounded by other cubes.  That should be the external 
 # surface area.
@@ -63,7 +67,7 @@ def is_exterior(data,exterior,cube):
     checked = set()
     todo = [cube]
     while todo:
-        cube = todo.pop()
+        cube = todo.pop(0)
         if cube in checked:
             continue
         checked.add(cube)
@@ -74,18 +78,13 @@ def is_exterior(data,exterior,cube):
             exterior.update( checked - data )
             return 1
         if cube not in data:
-            for delta in directions:
-                todo.append(add(cube,delta))
+            for near in around(cube):
+                todo.append(near)
     return 0
 
 def part2(data):
     exterior = set()
-    result = 0
-    for cube in data:
-        for delta in directions:
-            result += is_exterior(data, exterior, add(cube,delta))
-
-    return result
+    return sum( is_exterior(data,exterior,near) for cube in data for near in around(cube))
 
 print("Part 1:", part1(data))
 print("Part 2:", part2(data))
