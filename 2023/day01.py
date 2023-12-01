9eb362271236e17fd0ddc1089dda9a23fa927699
Tim Roberts <timr@probo.com>
2023-11-30 21:26:05 -0800

    Add day 1.

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,48 +1,53 @@
 import sys
+import re
 
 test = """\
-1000
-2000
-3000
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
 
-4000
+test2 = """\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
 
-5000
-6000
-
-7000
-8000
-9000
-
-10000"""
-
+nums = ['\\d','one','two','three','four','five','six','seven','eight','nine']
+pat = re.compile('|'.join(nums))
 
 if 'test' in sys.argv:
     data = test.splitlines()
 else:
     data = open('day01.txt').readlines()
 
+
 def part1(data):
-    accum = 0
-    maxx = 0
+    sumx = 0
     for line in data:
-        line = line.strip()
-        if not line:
-            maxx = max(maxx,accum)
-            accum = 0
-        else:
-            accum += int(line)
-    return maxx
+        dig = []
+        for c in line:
+            if c.isdigit():
+                dig.append(int(c))
+        sumx += dig[0]*10 + dig[-1]
+    return sumx
+
 
 def part2(data):
-    accum = [0]
+    sumx = 0
     for line in data:
-        line = line.strip()
-        if not line:
-            accum.append(0)
-        else:
-            accum[-1] += int(line)
-    return sum(sorted(accum)[-3:])
+        for z in nums[1:]:
+            line = line.replace(z,z[0]+z+z[-1])
+        p = pat.findall(line)
+        dig = [int(i) if i.isdigit() else nums.index(i) for i in p]
+        sumx += dig[0]*10 + dig[-1]
+    return sumx
+
 
 print("Part 1:", part1(data))
+if 'test' in sys.argv:
+    data = test2.splitlines()
 print("Part 2:", part2(data))
