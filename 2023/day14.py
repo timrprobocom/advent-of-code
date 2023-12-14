2f605e1f3bf07fe15bdad7a1b744fa7904b471e6
Tim Roberts <timr@probo.com>
2023-12-13 23:15:53 -0800

    Day 14 is WAY better with the grid as a list of lists.

--- 2023/day14.py
+++ 2023/day14.py
@@ -21,89 +21,102 @@ if 'test' in sys.argv:
 else:
     data = open(day+'.txt').read()
 
-
 DEBUG = 'debug' in sys.argv
 
-# Convert to a dict.
+# This is one case where the fancy data structure did not help.  I converted
+# the grid to a list of coordinate pairs.  69 seconds vs 0.5 seconds.
 
-round = []
-cube = set()
-for y,row in enumerate(data.splitlines()):
-    for x,c in enumerate(row):
-        if c == '#':
-            cube.add( (x,y) )
-        elif c == 'O':
-            round.append( (x,y) )
+def makegrid(data):
+    return [list(row) for row in data.splitlines()]
 
-H = y+1
-W = x+1
+grid = makegrid(data)
+H = len(grid)
+W = len(grid[0])
 
-def tilt_n(round):
-    round = sorted(round, key=lambda z: z[1])
-    newround = []
-    for x,y in round:
-        while y and (x,y-1) not in newround and (x,y-1) not in cube:
-            y -= 1
-        newround.append( (x,y) )
-    return newround
+def tilt_n(grid):
+    for x in range(W):
+        dy = 0
+        for y in range(H):
+            c = grid[y][x]
+            if c == '.':
+                dy += 1
+            elif c == '#':
+                dy = 0
+            elif c == 'O':
+                grid[y][x] = '.'
+                grid[y-dy][x] = 'O'
 
-def tilt_w(round):
-    # Sort by X.
-    round = sorted(round)
-    newround = []
-    for x,y in round:
-        while x and (x-1,y) not in newround and (x-1,y) not in cube:
-            x -= 1
-        newround.append( (x,y) )
-    return newround
+def tilt_s(grid):
+    for x in range(W):
+        dy = 0
+        for y in range(H-1,-1,-1):
+            c = grid[y][x]
+            if c == '.':
+                dy += 1
+            elif c == '#':
+                dy = 0
+            elif c == 'O':
+                grid[y][x] = '.'
+                grid[y+dy][x] = 'O'
 
-def tilt_s(round):
-    # Sort by Y reversed.
-    round = sorted(round, key=lambda z: -z[1])
-    newround = []
-    for x,y in round:
-        while y < H-1 and (x,y+1) not in newround and (x,y+1) not in cube:
-            y += 1
-        newround.append( (x,y) )
-    return newround
+def tilt_w(grid):
+    for y in range(H):
+        dx = 0
+        for x in range(W):
+            c = grid[y][x]
+            if c == '.':
+                dx += 1
+            elif c == '#':
+                dx = 0
+            elif c == 'O':
+                grid[y][x] = '.'
+                grid[y][x-dx] = 'O'
 
-def tilt_e(round):
-    # Sort by X reversed.
-    round = sorted(round, reverse=True)
-    newround = []
-    for x,y in round:
-        while x < W-1 and (x+1,y) not in newround and (x+1,y) not in cube:
-            x += 1
-        newround.append( (x,y) )
-    return newround
+def tilt_e(grid):
+    for y in range(H):
+        dx = 0
+        for x in range(W-1,-1,-1):
+            c = grid[y][x]
+            if c == '.':
+                dx += 1
+            elif c == '#':
+                dx = 0
+            elif c == 'O':
+                grid[y][x] = '.'
+                grid[y][x+dx] = 'O'
 
-def weight(round):
-    return sum(H-z[1] for z in round)
+def weight(grid):
+    return sum((H-y)*row.count('O') for y,row in enumerate(grid))
 
-def part1(round):
-    return weight(tilt_n(round))
+def unique(grid):
+    return hash(tuple([''.join(row) for row in grid]))
 
-def part2(round):
+def part1(data):
+    grid = makegrid(data)
+    tilt_n(grid)
+    return weight(grid)
+
+def part2(data):
+    grid = makegrid(data)
     seen = [0]
     scores = [0]
     want = -1
     for i in itertools.count():
-        round = tilt_n(round)
-        round = tilt_w(round)
-        round = tilt_s(round)
-        round = tilt_e(round)
-        cur = hash(tuple(round))
-        scores.append(weight(round))
+        tilt_n(grid)
+        tilt_w(grid)
+        tilt_s(grid)
+        tilt_e(grid)
+        cur = unique(grid)
+        scores.append(weight(grid))
         if DEBUG:
             print(i+1,scores[-1])
         if cur in seen:
-            pat0 = seen.index(cur)
-            cycle = len(seen) - pat0
-            want =  (1000000000 - pat0) % cycle + pat0
             break
         seen.append(cur)
+    pat0 = seen.index(cur)
+    cycle = len(seen) - pat0
+    want =  (1000000000 - pat0) % cycle + pat0
     return scores[want]
 
-
-print("Part 1:", part1(round)) # 64
-print("Part 2:", part2(round)) # 83516
+print("Part 1:", part1(data)) # 110821
+print("Part 2:", part2(data)) # 83516
