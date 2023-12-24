7afcb09f0b8d0e83be56f7ced3a89e2170d5175f
Tim Roberts <timr@probo.com>
2023-12-23 23:16:23 -0800

    Little bit of cleanup.

--- 2023/day24.py
+++ 2023/day24.py
@@ -3,6 +3,7 @@ import re
 import sys
 import collections
 from sympy import solve_poly_system, Symbol
+import numpy as np
 
 test = """\
 19, 13, 30 @ -2,  1, -2
@@ -13,46 +14,50 @@ test = """\
 
 day = os.path.splitext(os.path.basename(__file__))[0]
 
-if 'test' in sys.argv:
+TEST = 'test' in sys.argv
+DEBUG = 'debug' in sys.argv
+
+if TEST:
     data = test.strip()
 else:
     data = open(day+'.txt').read().strip()
 
 Point = collections.namedtuple('Point',('x','y','z','dx','dy','dz','m','b'))
 
+nums = re.compile(r"-*\d+")
+def get_all_nums(line):
+    return tuple(int(i) for i in nums.findall(row))
+
 def find_mbx(x0,y0,dx,dy):
     m = dy/dx
-    b =  y0 - m * x0
+    b = y0 - m * x0
     return (m,b)
 
-nums = re.compile(r"-*\d+")
 vectors = []
 for row in data.splitlines():
-    n = tuple(int(i) for i in nums.findall(row))
+    n = get_all_nums(row)
     n = n + find_mbx(n[0],n[1],n[3],n[4])
     vectors.append( Point(*n))
 
-DEBUG = 'debug' in sys.argv
-
 def intersect2d(pt1,pt2):
-    # So, where does m0 x + b0 = m1 x + b1?
+    # Are the lines parallel?
     if pt1.m == pt2.m:
         return (0,0)
+    # So, where does m0 x + b0 = m1 x + b1?
     x = (pt2.b-pt1.b)/(pt1.m-pt2.m)
     y = pt1.m*x+pt1.b
-    # Is this in the past?
-    if (pt1.dx > 0 and x < pt1.x) or (pt1.dx < 0 and x > pt1.x) or (pt2.dx > 0 and x < pt2.x) or (pt2.dx < 0 and x > pt2.x):
+    # Did this happen in the past?
+    if ((pt1.dx > 0) == (x < pt1.x)) or ((pt2.dx > 0) == (x < pt2.x)):
         return (0,0)
     return (x,y)
 
 def part1(vectors):
-    rmin,rmax = (7,28) if 'test' in sys.argv else (200000000000000,400000000000000)
+    rmin,rmax = (7,28) if TEST else (2*10**14,4*10**14) 
     count = 0
     for i,pt0 in enumerate(vectors):
         for pt1 in vectors[i+1:]:
             x,y = intersect2d(pt0,pt1)
-            if x:
-                count += rmin <= x <= rmax and rmin < y < rmax
+            count += rmin <= x <= rmax and rmin < y < rmax
     return count
 
 def part2(vectors):
@@ -61,7 +66,7 @@ def part2(vectors):
     # It's a system of linear equations, right?  For the first three vectors:
     # x + dx*t == vx + vdx * t
     # y + dy*t == vy + vdy * t
-    # Z + dz*t == vz + vdz * t
+    # z + dz*t == vz + vdz * t
 
     x = Symbol('x')
     y = Symbol('y')
@@ -80,7 +85,6 @@ def part2(vectors):
     if DEBUG:
         print(result)
     return sum(result[0][0:3])
-    
 
 print("Part 1:", part1(vectors))
 print("Part 2:", part2(vectors))
