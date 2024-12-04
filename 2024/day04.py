632ccd653a4ae770c60c80ccc78012d84aeb5f0e
Tim Roberts <timr@probo.com>
2024-12-03 21:59:54 -0800

    Minor tweak to day 4.

--- 2024/day04.py
+++ 2024/day04.py
@@ -29,12 +29,10 @@ HEIGHT = len(data)
 # Find all occurrances of c.
 
 def findall(c):
-    xx = []
-    for y,row in enumerate(data):
-        for x,col in enumerate(row):
-            if col==c:
-                xx.append((x,y))
-    return xx
+    return [(x,y) 
+        for y,row in enumerate(data)
+        for x,col in enumerate(row)
+        if col==c]
                
 dirs = ( (-1,-1), (-1,0), (-1,1),
          ( 0,-1),         (0,1),
@@ -57,16 +55,13 @@ def part1(data):
 def part2(data):
     winner = 0
     for x0,y0 in findall('A'):
-        if not (0 < x0 < WIDTH-1) or not (0 < y0 < HEIGHT-1):
-            continue
-        winner += (
+        winner += (0 < x0 < WIDTH-1) and (0 < y0 < HEIGHT-1) and (
               (data[y0-1][x0-1] == 'M' and  data[y0+1][x0+1] == 'S') or 
               (data[y0-1][x0-1] == 'S' and  data[y0+1][x0+1] == 'M')
         ) and (
               (data[y0-1][x0+1] == 'M' and  data[y0+1][x0-1] == 'S') or 
               (data[y0-1][x0+1] == 'S' and  data[y0+1][x0-1] == 'M')
-        )#:
-        #    winner += 1
+        )
     return winner
 
 print("Part 1:", part1(data))
