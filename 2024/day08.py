c2c470cb8a688ed027e195a8d1a1ba929d8e30e8
Tim Roberts <timr@probo.com>
2024-12-07 22:01:35 -0800

    More streamlining.

--- 2024/day08.py
+++ 2024/day08.py
@@ -2,6 +2,7 @@ import os
 import sys
 from pprint import pprint
 from collections import defaultdict
+from itertools import permutations
 
 test = """\
 ............
@@ -45,13 +46,12 @@ def printgrid(antinodes):
 
 def part1(spots):
     antinodes = set()
-    for k,v in spots.items():
-        for i,(x0,y0) in enumerate(v):
-            for x1,y1 in v[i+1:]:
-                dx = x1-x0
-                dy = y1-y0
-                antinodes.add( (x0-dx,y0-dy))
-                antinodes.add( (x1+dx,y1+dy))
+    for v in spots.values():
+        for (x0,y0),(x1,y1) in permutations(v,2):
+            dx = x1-x0
+            dy = y1-y0
+            antinodes.add( (x0-dx,y0-dy))
+            antinodes.add( (x1+dx,y1+dy))
     antinodes = set((x,y) for (x,y) in antinodes if x in range(WIDTH) and y in range(HEIGHT))
     if DEBUG:
         printgrid(antinodes)
@@ -59,21 +59,18 @@ def part1(spots):
 
 def part2(data):
     antinodes = set()
-    for k,v in spots.items():
-        for i,(xx0,yy0) in enumerate(v):
-            for x1,y1 in v[i+1:]:
-                x0 = xx0
-                y0 = yy0
-                dx = x1-x0
-                dy = y1-y0
-                while x0 in range(WIDTH) and y0 in range(HEIGHT):
-                    antinodes.add( (x0,y0) )
-                    x0 -= dx
-                    y0 -= dy
-                while x1 in range(WIDTH) and y1 in range(HEIGHT):
-                    antinodes.add( (x1,y1) )
-                    x1 += dx
-                    y1 += dy
+    for v in spots.values():
+        for (x0,y0),(x1,y1) in permutations(v,2):
+            dx = x1-x0
+            dy = y1-y0
+            while x0 in range(WIDTH) and y0 in range(HEIGHT):
+                antinodes.add( (x0,y0) )
+                x0 -= dx
+                y0 -= dy
+            while x1 in range(WIDTH) and y1 in range(HEIGHT):
+                antinodes.add( (x1,y1) )
+                x1 += dx
+                y1 += dy
     if DEBUG:
         printgrid(antinodes)
     return len(antinodes)
