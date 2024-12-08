ccf3cdae505fc57f54f2d9512017fa1d9e3b3d6c
Tim Roberts <timr@probo.com>
2024-12-07 21:44:40 -0800

    Streamline day 8.

--- 2024/day08.py
+++ 2024/day08.py
@@ -34,7 +34,14 @@ spots = defaultdict(list)
 for y,row in enumerate(data):
     for x,c in enumerate(row):
         if c != '.':
-            spots[c].append( (x,y))
+            spots[c].append((x,y))
+
+def printgrid(antinodes):
+    grid = [list(row) for row in data]
+    for x,y in antinodes:
+        grid[y][x] = '#'
+    for row in grid:
+        print(''.join(row)) 
 
 def part1(spots):
     antinodes = set()
@@ -43,44 +50,32 @@ def part1(spots):
             for x1,y1 in v[i+1:]:
                 dx = x1-x0
                 dy = y1-y0
-                an = (
-                    (x0-dx,y0-dy),
-                    (x1+dx,y1+dy)
-                )
-                print(k,x0,y0,x1,y1,an)
-                for x,y in an:
-                    if x in range(WIDTH) and y in range(HEIGHT):
-                        antinodes.add((x,y))
-    print(antinodes)
+                antinodes.add( (x0-dx,y0-dy))
+                antinodes.add( (x1+dx,y1+dy))
+    antinodes = set((x,y) for (x,y) in antinodes if x in range(WIDTH) and y in range(HEIGHT))
+    if DEBUG:
+        printgrid(antinodes)
     return len(antinodes)
-
-def printgrid(antinodes):
-    grid = [list('.'*WIDTH) for _ in range(HEIGHT)]
-    for x,y in antinodes:
-        grid[y][x] = '#'
-    for row in grid:
-        print(''.join(row)) 
 
 def part2(data):
     antinodes = set()
     for k,v in spots.items():
         for i,(xx0,yy0) in enumerate(v):
             for x1,y1 in v[i+1:]:
-                x0=xx0
-                y0=yy0
-                print('start',x0,y0,x1,y1)
+                x0 = xx0
+                y0 = yy0
                 dx = x1-x0
                 dy = y1-y0
                 while x0 in range(WIDTH) and y0 in range(HEIGHT):
-                    print(k,x0,y0)
                     antinodes.add( (x0,y0) )
                     x0 -= dx
                     y0 -= dy
                 while x1 in range(WIDTH) and y1 in range(HEIGHT):
-                    print(k,x1,y1)
                     antinodes.add( (x1,y1) )
                     x1 += dx
                     y1 += dy
+    if DEBUG:
+        printgrid(antinodes)
     return len(antinodes)
 
 print("Part 1:", part1(spots))
