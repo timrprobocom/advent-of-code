6cfd37521942fb092dff38f957b50e5cfb9f99e4
Tim Roberts <timr@probo.com>
2024-12-08 22:15:17 -0800

    Day 9 works.  This one bugged me.

---
+++ 2024/day09.py
@@ -0,0 +1,130 @@
+import os
+import sys
+from pprint import pprint
+from collections import defaultdict
+from itertools import permutations
+
+test = """\
+2333133121414131402"""
+
+day = os.path.splitext(os.path.basename(__file__))[0]
+
+TEST = 'test' in sys.argv
+DEBUG = 'debug' in sys.argv
+
+if TEST:
+    data = test.strip()
+else:
+    data = open(day+'.txt').read().strip()
+
+data = [int(c) for c in data]
+
+def part1(data):
+    idn = 0
+    disk = []
+
+    for i,c in enumerate(data):
+        if i % 2:
+            disk.extend( [-1]*c)
+        else:
+            disk.extend( [idn]*c)
+            idn += 1
+
+    # Now compact it.
+    i = 0
+    j = len(disk)-1
+
+    while i < j:
+        while disk[i] >= 0:
+            i += 1
+        while disk[j] < 0:
+            j -= 1
+        if i >= j:
+            break
+        disk[i], disk[j] = disk[j], -1
+        i += 1
+        j -= 1
+    
+    if DEBUG:
+        print(disk)
+
+    # Evaluate.
+
+    sumx = 0
+    for n,i in enumerate(disk):
+        if i >= 0:
+            sumx += n*i
+    return sumx
+
+def coalesce( space, start, size ):
+    newspace = []
+    while space:
+        sstart, ssize = space.pop(0)
+        if not ssize:
+            continue
+        elif start > sstart+ssize:
+            newspace.append( [sstart, ssize] )
+        elif start+size == sstart:
+            newspace.append( [start, size+ssize] )
+            break
+        elif sstart+ssize == start:
+            newspace.append( [sstart, size+ssize] )
+            break
+        elif sstart > start+size:
+            break
+    return newspace+space
+
+
+def find_hole_below( space, start, size ):
+    for i,s in enumerate(space):
+        if s[0] > start:
+            return -1
+        if s[1] >= size:
+            return i
+    return -1
+
+def part2(data):
+    idn = 0
+    locate = 0
+    files = []
+    space = []
+
+    for i,c in enumerate(data):
+        if i % 2:
+            space.append( [locate, c])
+        else:
+            files.append( [locate, idn , c] )
+            idn += 1
+        locate += c
+
+    if DEBUG:
+        print(files)
+        print(space)
+
+    idnmax = idn
+
+    for idn in range(idnmax-1,-1,-1):
+        start, _, size = files[idn]
+        n = find_hole_below( space, start, size )
+        if n >= 0:
+            files[idn][0] = space[n][0]
+            space[n][0] += size
+            space[n][1] -= size
+            space = coalesce(space, start, size)
+    
+    # Sort the files by location.
+
+    files.sort()
+    if DEBUG:
+        print(files)
+        print(space)
+
+    # Evaluate.
+
+    sumx = 0
+    for start, idn, size in files:
+        sumx += sum(range(start,start+size)) * idn
+    return sumx
+
+print("Part 1:", part1(data))
+print("Part 2:", part2(data))
