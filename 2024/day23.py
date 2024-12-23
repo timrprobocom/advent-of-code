1c44c707294b9fb8d2cc92fe03fc618a66b57f9a
Tim Roberts <timr@probo.com>
2024-12-22 23:12:37 -0800

    Day 23 works.

---
+++ 2024/day23.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import math
+from collections import defaultdict, Counter
+from itertools import permutations
+
+test = """\
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn"""
+
+day = os.path.splitext(os.path.basename(__file__))[0]
+
+TEST = 'test' in sys.argv
+DEBUG = 'debug' in sys.argv
+
+if TEST:
+    data = test
+else:
+    data = open(day+'.txt').read()
+
+pairs = [line.split('-') for line in data.splitlines()]
+
+def part1(data):
+    connx = defaultdict(set)
+    for a,b in data:
+        connx[a].add(b)
+        connx[b].add(a)
+    uniq = set()
+    for k,v in connx.items():
+        for v1 in v:
+            for v2 in v:
+                if k[0] == 't' or v1[0] == 't' or v2[0] == 't':
+                    if v1 != v2 and k in connx[v2] and v1 in connx[v2]:
+                        uniq.add( tuple(sorted((k,v1,v2))))
+    return len(uniq)
+
+def part2(data):
+    connx = defaultdict(set)
+    for a,b in data:
+        connx[a].add(a)
+        connx[b].add(b)
+        connx[a].add(b)
+        connx[b].add(a)
+
+    matches = ((a & b) for a,b in permutations(connx.values(),2))
+    poss = []
+    for m in matches:
+        if len(m) < 3:
+            continue
+        base = set.intersection( *(connx[n] for n in m) )
+        if base:
+            poss.append(base)
+
+    # Keep the longest sequences.
+
+    maxx = max(len(p) for p in poss)
+    print(maxx)
+    poss = [p for p in poss if len(p)==maxx]
+    print(len(poss))
+    # I don't know why there are maxes other than our winner, but the most common is the answer.
+    counts = Counter(','.join(sorted(p)) for p in poss)
+    print(counts.most_common())
+    return [k for k,v in counts.items() if v > 2][0]
+
+print("Part 1:", part1(pairs))
+print("Part 2:", part2(pairs))
