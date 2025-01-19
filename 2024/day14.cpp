33e8ed67dd2154ab196ab825173b1cfe17582161
Tim Roberts <timr@probo.com>
2025-01-18 21:06:57 -0800

    Day 14 works in C++.

--- 2024/day14.cpp
+++ 2024/day14.cpp
@@ -3,13 +3,11 @@
 #include <sstream>
 #include <string>
 #include <iterator>
+#include <cmath>
 #include <cstdint>
 #include <cstring>
-#include <cmath>
 #include <vector>
 #include <map>
-#include <set>
-#include <queue>
 #include <algorithm>
 #include <numeric>
 
@@ -34,34 +32,26 @@ const string test(
 
 bool DEBUG = false;
 bool TEST = false;
+#define DAY "day14"
 
-int WIDTH = -1;
-int HEIGHT = -1;
+int WIDTH=-1;
+int HEIGHT=-1;
 
-typedef vector<vector<int>> IntMatrix;
-
-void printdata( IntMatrix & data )
+void printgrid(IntMatrix & data)
 {
-    for( auto & row : data )
-    {
-        cout << row[0] << " " << row[1] << " " << row[2] << " " << row[3] << "\n";
+    uint8_t grid[HEIGHT][WIDTH] = {0};
+    bzero( grid, WIDTH*HEIGHT );
+    for( auto & bot : data )
+        grid[bot[1]][bot[0]] += 1;
+    for( int y=0; y < HEIGHT; y++ )
+    {   
+        for( int x=0; x < WIDTH; x++ )
+            cout << ".1234"[grid[y][x]];
+        cout << endl;
     }
 }
 
-
-void printgrid( IntMatrix & data )
-{
-    vector<string> grid;
-    for( int i = 0; i < HEIGHT; i++ )
-        grid.push_back( string(WIDTH, '0') );
-    for( auto & row : data )
-        grid[row[1]][row[0]] ++;
-    for( auto & row : grid )
-        cout << row << "\n";
-}
-
-
-void move( IntMatrix & data )
+void move(IntMatrix & data)
 {
     for( auto & row : data )
     {
@@ -70,102 +60,72 @@ void move( IntMatrix & data )
     }
 }
 
-
-double calculateStandardDeviation(const vector<int> & arr)
-{
-    double sum = 0.0, mean = 0.0, standardDeviation = 0.0;
-
-    int size = arr.size();
-
-    // Calculate the sum of elements in the vector.
-
-    for( auto n : arr )
-        sum += n;
-
-    // Calculate the mean
-
-    mean = sum / size;
-
-    // Calculate the sum of squared differences from the mean.
-
-    for( auto n : arr )
-        standardDeviation += (n-mean) * (n-mean);
-
-    // Calculate the square root of the variance to get the
-    // standard deviation
-
-    return sqrt(standardDeviation / size);
-}
-
-
-// The tree is solid, so the standard deviation of the coordinates goes WAY down.
-// Typical is 30, tree gets 19.
-
-bool detect_tree( IntMatrix & data )
-{
-    vector<int> column;
-    for( int i = 0; i < 2; i++ )
-    {
-        transform( 
-            data.begin(),
-            data.end(),
-            back_inserter( column ),
-            [i]( vector<int> & row ) { return row[i]; }
-        );
-
-        double x = calculateStandardDeviation(column);
-        if( x > 24.0 )
-            return false;
-    }
-    if( DEBUG )
-        printgrid(data);
-    return true;
-}
-
-
-int64_t part1( IntMatrix & data )
+int64_t part1(IntMatrix & data)
 {
     for( int i = 0; i < 100; i++ )
-        move( data );
+        move(data);
 
     int hw = WIDTH/2;
     int hh = HEIGHT/2;
-    int k1=0, k2=0, k3=0, k4=0;
+    int k[4] = {0,0,0,0};
     for( auto & row : data )
     {
-        if( row[0] < hw )
-        {
-            if( row[1] < hh )
-                k1++;
-            else if( row[1] > hh )
-                k2++;
-        }
-        else if( row[0] > hw )
-        {
-            if( row[1] < hh )
-                k3++;
-            else if( row[1] > hh )
-                k4++;
-        }
+        if( row[0] < hw && row[1] < hh )
+            k[0]++;
+        if( row[0] > hw && row[1] < hh )
+            k[1]++;
+        if( row[0] < hw && row[1] > hh )
+            k[2]++;
+        if( row[0] > hw && row[1] > hh )
+            k[3]++;
     }
-
-    return k1*k2*k3*k4;
+    return k[0]*k[1]*k[2]*k[3]; 
 }
 
-int64_t part2( IntMatrix & data )
+// The tree is solid, so the standard deviation of the coordinates goes WAY down.
+// Tpyical is 30, tree gets 19.
+
+bool detect_tree( IntMatrix & data )
 {
-    for( int i = 101; i < 10000; i++ )
+    int n = data.size();
+    float sumx = 0;
+    float sumx2 = 0;
+    float sumy = 0;
+    float sumy2 = 0;
+    for( auto & row : data )
     {
-        move( data );
-        if( detect_tree(data) )
-            return i;
+        sumx += row[0];
+        sumx2 += row[0]*row[0];
+        sumy += row[1];
+        sumy2 += row[1]*row[1];
     }
-    return -1;
+    float meanx = sumx / n;
+    float meany = sumy / n;
+    float stdx = sqrt((sumx2 / n) - (meanx * meanx));
+    float stdy = sqrt((sumy2 / n) - (meany * meany));
+
+    return stdx < 20 && stdy < 20;
+}  
+
+
+int64_t part2(IntMatrix & data)
+{
+    for( int i = 100; i < 10000; i++ )
+    {
+        move(data);
+        if( detect_tree(data) )
+        {
+            if( DEBUG )
+                printgrid(data);
+            return i+1;
+        }
+    }
+    return 0;
 }
+
 
 int main( int argc, char ** argv )
 {
-    string name = *argv;
     while( *++argv )
     {
         string arg(*argv);
@@ -175,6 +135,7 @@ int main( int argc, char ** argv )
             TEST = true;
     }
 
+    string input = TEST ? test : file_contents(DAY".txt");
     if( TEST )
     {
         WIDTH = 11;
@@ -186,36 +147,42 @@ int main( int argc, char ** argv )
         HEIGHT = 103;
     }
 
-    string input = TEST ? test : file_contents("day14.txt");
+    // For parsing, each line has four numbers, possibly with a sign.
 
     IntMatrix data(1);
-    int sign = 1;
     int accum = 0;
-    bool more = false;
+    int state = 0;
     for( char c : input )
     {
         if( c == '-' )
         {
-            sign = -1;
+            state = -1;
         }
-        else if( '0' <= c && c  <= '9' )
+        else if( isdigit(c) )
         {
-            accum = accum * 10 + (c - '0');
-            more = true;
+            if( !state )
+                state = 1;
+            accum = accum * 10 + c - '0';
         }
-        else if( more )
+        else
         {
-            data.back().push_back( sign*accum );
-            sign = 1;
-            accum = 0;
-            more = false;
+            if( state )
+            {
+                data.back().push_back( accum * state );
+                accum = 0;
+                state = 0;
+            }
             if( c == '\n' )
                 data.push_back( vector<int>() );
         }
     }
-    data.back().push_back( sign*accum );
+
+    if( accum )
+        data.back().push_back(accum * state);
 
     cout << "Part 1: " << part1(data) << "\n";
+    if( DEBUG )
+        printgrid( data );
     if( !TEST )
         cout << "Part 2: " << part2(data) << "\n";
 }
