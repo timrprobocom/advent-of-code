f80a0dc6904f1e59cc63bb2a03db337072d23896
Tim Roberts <timr@probo.com>
2025-12-09 00:26:56 -0800

    Day 9 now down to 3 seconds.

--- 2025/day09.py
+++ 2025/day09.py
@@ -133,5 +133,36 @@ def part2(data):
                 best = area
     return best
 
+# This one takes 3 seconds.
+
+def part2(data):
+    best = 0
+    for a,b in itertools.combinations(data,2):
+        area = (abs( b[1]-a[1]) + 1) * (abs(b[0]-a[0])+1)
+        if area < best:
+            continue
+        x1,x2 = sorted((a[0],b[0]))
+        y1,y2 = sorted((a[1],b[1]))
+        maybe = True
+        for p1,p2 in zip(data, data[1:]+data[:1]):
+            if p1[0] == p2[0]:
+                # Vertical.
+                py0,py1 = sorted((p1[1],p2[1]))
+                if x1 < p1[0] < x2 and py0 <= y2 and py1 >= y1:
+                    maybe = False
+                    break
+            else:
+                # Horizontal.
+                px0,px1 = sorted((p1[0],p2[0]))
+                if y1 < p1[1] < y2 and px0 <= x2 and px1 >= x1:
+                    maybe = False
+                    break
+        if maybe:
+            if DEBUG:
+                print(area,a,b)
+            best = area
+    return best
+                
+
 print("Part 1:", part1(data))
 print("Part 2:", part2(data)) # 1429075575
